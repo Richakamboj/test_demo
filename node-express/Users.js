42fbd990f20a2c651f405ec285a54cbebd65c517
Richa kamboj <richakamboj416@gmail.com>
2023-08-05 10:49:29 +0530

    node express practise

---
+++ node-express/Users.js
@@ -0,0 +1,28 @@
+const users =[
+    {
+        id:1,
+        name:'john',
+        email:'jone@gmail.com'
+
+    },
+    {
+        id:2,
+        name:'richa',
+        email:'richa@gmail.com'
+
+    },
+    {
+        id:3,
+        name:'vineet',
+        email:'vineet@gmail.com'
+
+    },
+    {
+        id:4,
+        name:'vinRich',
+        email:'vinRich@gmail.com'
+
+    }
+]
+
+module.exports = users;
